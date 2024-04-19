0c23191e32673db82ce5451ef26ed548aa24e7be
LutragoacliffordIT24A <20211254@nbsc.edu.ph>
2024-04-19 08:35:45 +0800

    First Commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar 
+
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,7 +19,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        
       </IonContent>
     </IonPage>
   );
